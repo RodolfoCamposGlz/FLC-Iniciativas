fa7edd7da22bdb6a820ef128669bb3dcda8a04e6
RodolfoCamposGlz <rodolfocampos459@gmail.com>
2020-05-31 19:47:37 -0500

    cambios build

--- src/pages/colaboraDetails.jsx
+++ src/pages/colaboraDetails.jsx
@@ -1,18 +1,28 @@
 /* eslint-disable react/prop-types */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import LabsParticipar from '../components/labsParticipar/labsParticipar'
 import LabsCiudadanos from '../components/labsCiudadanos/labsCiudadanos'
 
 // eslint-disable-next-line react/prop-types
-const ColaboraDetails = ({ location }) => (
-  <Layout>
-    <SEO title="Foro" />
-    <div>{location.state.choice}</div>
-    <LabsCiudadanos />
-    <LabsParticipar />
-  </Layout>
-)
+const ColaboraDetails = ({ location }) => {
+  const [locationLink, setLocationLink] = useState('')
+
+  useEffect(() => {
+    if (location.state.choice !== undefined) {
+      setLocationLink(location.state.choice)
+    }
+  }, [])
+  return (
+    <Layout>
+      <SEO title="Foro" />
+      <div>{locationLink}</div>
+
+      <LabsCiudadanos />
+      <LabsParticipar />
+    </Layout>
+  )
+}
 
 export default ColaboraDetails
